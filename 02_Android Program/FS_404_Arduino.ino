f0fa8dc0c1343b05497a91145d8f51620fec6e70
Romain Dereu <rdereu@gmail.com>
2024-12-03 12:39:06 +0900

    Debouncing
    Adding Debouncing to the code of the Arduino

--- 02_Android Program/FS_404_Arduino.ino
+++ 02_Android Program/FS_404_Arduino.ino
@@ -5,33 +5,33 @@
 #include <EEPROM.h>
 
 // The pins for the selector switch
-int leftSwitchPin = 3;
-int rightSwitchPin = 2;
+const int leftSwitchPin = 3;
+const int rightSwitchPin = 2;
 
 // The main on off swich
 int onOffSwitchPin = 5;
-int onOffSwitchLast = 1;
+bool previousButtonSwitch;
 
 // The pins for the led
-int led1 = 10;
-int led2 = 9;
-int led3 = 8;
-int ledon = 7;
+const int led1 = 10;
+const int led2 = 9;
+const int led3 = 8;
+const int ledon = 7;
 // Switch leds are the three that change according to the switch
-int switchLedNumbers[] = {led1, led2, led3};
+const int switchLedNumbers[] = {led1, led2, led3};
 
 // The notes that will be played. Read from Eeprom
-int cmd1 = EEPROM.read(1);
-int pitch1 = EEPROM.read(2);
-int velocity1 = EEPROM.read(3);
+const int cmd1 = EEPROM.read(1);
+const int pitch1 = EEPROM.read(2);
+const int velocity1 = EEPROM.read(3);
 
-int cmd2 = EEPROM.read(4);
-int pitch2 = EEPROM.read(5);
-int velocity2 = EEPROM.read(6);
+const int cmd2 = EEPROM.read(4);
+const int pitch2 = EEPROM.read(5);
+const int velocity2 = EEPROM.read(6);
 
-int cmd3 = EEPROM.read(7);
-int pitch3 = EEPROM.read(8);
-int velocity3 = EEPROM.read(9);
+const int cmd3 = EEPROM.read(7);
+const int pitch3 = EEPROM.read(8);
+const int velocity3 = EEPROM.read(9);
 
 
 void setup() {
@@ -58,6 +58,8 @@ void setup() {
   Serial.end();
   */
 
+
+
   
 
   Serial.begin(31250);
@@ -76,8 +78,6 @@ void setup() {
   digitalWrite(ledon, HIGH);
 }
 
-
-
 int switchposition() {
     //Reading the state of both the switch and the on off button
     int leftSwitchRead = digitalRead(leftSwitchPin);
@@ -85,14 +85,16 @@ int switchposition() {
     if (leftSwitchRead == HIGH && rightSwitchRead == LOW) {return 0;}
     else if (leftSwitchRead == HIGH && rightSwitchRead == HIGH) {return 1;}
     else if (leftSwitchRead == LOW && rightSwitchRead == HIGH) {return 2;}
-    else{return 3;}
+    else{return;}
   }
 
+//Romain delete for production
 void updatelight(int toggleSwitchPosition) {
   for(int i=0; i<=2; i++){    
     if (i == toggleSwitchPosition) {digitalWrite(switchLedNumbers[i], HIGH); }
     else {digitalWrite(switchLedNumbers[i], LOW);}
     }
+  return;
   }
 
 // plays a MIDI note. Doesn't check to see that cmd is greater than 127, or that
@@ -101,28 +103,52 @@ void noteOn(int cmd, int pitch, int velocity) {
   Serial.write(cmd);
   Serial.write(pitch);
   Serial.write(velocity);
+  return;
   }
 
 void sendmidi(int toggleSwitchPosition) {
   if (toggleSwitchPosition == 0){noteOn(cmd1, pitch1, velocity1);}
-
   else if (toggleSwitchPosition == 1){noteOn(cmd2, pitch2, velocity2);}
-
   else if (toggleSwitchPosition == 2){noteOn(cmd3, pitch3, velocity3);}
+  return;
+  }
 
+bool debounce (bool previousButtonSwitch) {
+  if(previousButtonSwitch == LOW){
+    return 0;
+    }
+
+  else
+  {
+    delay(10);
+    bool currentState = digitalRead(onOffSwitchPin);
+    if(currentState == LOW){return 1;}
+    else{return 0;}
+    }
   }
 
 void loop() {
+  //Checks whether Serial is connected. If this is the case, the pedal will be in update mode
+  //The loop is infinite so the pedal needs to be reset after the values are updated
+  if (Serial.available()) {
+    for(; ;){
+      Serial.read();
+      }
+    }
+
+
   // Checking the toggle and on off switches
   int toggleSwitchPosition = switchposition();
-  int onOffSwitchRead = digitalRead(onOffSwitchPin);
+  bool currentSwitchPosition = digitalRead(onOffSwitchPin);
 
   //updating the light value and sending midi if the note is on
+  //Romain delete for production
   updatelight(toggleSwitchPosition);
-  
-    if (onOffSwitchRead == LOW && onOffSwitchLast != LOW){
-        sendmidi(toggleSwitchPosition);
-        delay(500);
-          }
-  onOffSwitchLast = onOffSwitchRead;
+
+
+    if (currentSwitchPosition == 0 && debounce(previousButtonSwitch)){
+          sendmidi(toggleSwitchPosition);
+          delay(200);
+            }
+  previousButtonSwitch = digitalRead(onOffSwitchPin);
 }
